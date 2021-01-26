95b7fff5a49a6e08ead262f9ab0cad4d3c165887
Xavier Portilla Edo <xavi_tb@hotmail.com>
2021-01-26 20:12:15 +0100

    ci(circleci): added sonar and git hooks (CORE-4768) (#3)
    * ci(circleci): added sonar and git hooks
    
    * ci(circleci): added yarn file
    
    * fix: move pr check to circleci and eslint finished
    
    * fix: fix unit test lint

--- tests/verror.unit.js
+++ tests/verror.unit.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/* eslint-disable no-unused-expressions */
 const { expect } = require('chai');
 
 const HttpStatus = require('http-status');
@@ -40,7 +39,7 @@ describe('VError unit tests', () => {
     const error = await throws().catch((err) => err);
     expect(error.message).to.eql('boom');
     expect(error.code).to.eql(HttpStatus.INTERNAL_SERVER_ERROR);
-    expect(error.stack).to.not.be.undefined;
+    expect(error.stack).to.not.eql(undefined);
     expect(error.name).to.eql('verror');
     expect(error.dateTime.valueOf()).to.be.within(Date.now() - 10, Date.now() + 10);
     expect(error.data).to.eql(undefined);
@@ -54,7 +53,7 @@ describe('VError unit tests', () => {
     const error = await throws().catch((err) => err);
     expect(error.message).to.eql('boom');
     expect(error.code).to.eql(HttpStatus.BAD_REQUEST);
-    expect(error.stack).to.not.be.undefined;
+    expect(error.stack).to.not.eql(undefined);
     expect(error.name).to.eql('verror');
     expect(error.dateTime.valueOf()).to.be.within(Date.now() - 10, Date.now() + 10);
     expect(error.data).to.eql(undefined);
@@ -68,7 +67,7 @@ describe('VError unit tests', () => {
     const error = await throws().catch((err) => err);
     expect(error.message).to.eql('boom');
     expect(error.code).to.eql(HttpStatus.INTERNAL_SERVER_ERROR);
-    expect(error.stack).to.not.be.undefined;
+    expect(error.stack).to.not.eql(undefined);
     expect(error.name).to.eql('verror');
     expect(error.dateTime.valueOf()).to.be.within(Date.now() - 10, Date.now() + 10);
     expect(error.data).to.eql({ foo: 'bar' });
